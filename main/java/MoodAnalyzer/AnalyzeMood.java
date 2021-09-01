4cb9576496b6b6b3f8b451758601f7ca75638c51
snehak2299 <skanase2299@gmail.com>
2021-09-01 23:52:38 +0530

    [sneha]:invalid mood that is null

--- main/java/MoodAnalyzer/AnalyzeMood.java
+++ main/java/MoodAnalyzer/AnalyzeMood.java
@@ -6,15 +6,23 @@ public class AnalyzeMood {
 	public AnalyzeMood(String msg) {   //refactored
 		this.msg = msg;
 	}
-	public String analyseMood () {
-		if(msg.contains("happy")) { 	//uc1
-			return "happy";
+	public String analyseMood () throws Exception {
+		try {
+			if(msg.contains("happy")) { 	//uc1
+				return "happy";
+			}
+			else if(msg.contains("sad")) { //tc1
+				return "sad";
+			}
+			else if(msg.length()<=0) {
+				throw new Exception("Mood cannot be empty");
+				}
+			else {							//tc2
+				return "happy";
+			}
 		}
-		else if(msg.contains("sad")) { //tc1
-			return "sad";
-		}
-		else {							//tc2
-			return "happy";
+		catch(NullPointerException exception) { //exception
+			return "Happy";
 		}
 	}
 
--- test/java/MoodAnalyzer/MoodAnalyzerTest.java
+++ test/java/MoodAnalyzer/MoodAnalyzerTest.java
@@ -10,21 +10,49 @@ public class MoodAnalyzerTest {
 	@Test
 	public void givenMood_whenhappy_shouldReturnHappy() {
 		AnalyzeMood moodAnalizer =new AnalyzeMood("I am in happy mood");
-		String mood=moodAnalizer.analyseMood();
-		assertEquals("happy",mood);
+		String mood = null;
+		try {
+			mood = moodAnalizer.analyseMood();
+		} catch (Exception e) {
+			e.printStackTrace();
+			assertEquals("happy",e.getMessage());
+		}
+		
 	}
 	@Test
 	public void givenMood_whensad_shouldReturnSad() {  //repeat 1.1
 		AnalyzeMood moodAnalizer =new AnalyzeMood("I am in sad mood");
-		String mood=moodAnalizer.analyseMood();
-		assertEquals("sad",mood);
+		String mood;
+		try {
+			mood = moodAnalizer.analyseMood();
+		} catch (Exception e) {
+			assertEquals("sad",e.getMessage());
+			e.printStackTrace();
+		}
 	}
 	@Test
 	public void givenMood_whenAnyMood_shouldReturnHappy() {  //repeat 1.2
 		AnalyzeMood moodAnalizer =new AnalyzeMood("I am in any mood");
-		String mood=moodAnalizer.analyseMood();
-		assertEquals("happy",mood);
+		String mood;
+		try {
+			mood = moodAnalizer.analyseMood();
+		} catch (Exception e) {
+			assertEquals("happy",e.getMessage());
+			e.printStackTrace();
+		}
+		
 	}
-
+	@Test
+	public void givenMood_whennull_shouldReturnHappy() {  //repeat 2.1
+		AnalyzeMood moodAnalizer =new AnalyzeMood(null);
+		String mood;
+		try {
+			mood = moodAnalizer.analyseMood();
+		} catch (Exception e) {
+			assertEquals("mood is null",e.getMessage());
+			e.printStackTrace();
+		}
+		
+	}	
 	
 }
